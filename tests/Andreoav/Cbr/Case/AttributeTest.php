75c55e7f93c99c7707a12d814fc349fe7d669b64
Andreo Vieira <andreoav@gmail.com>
2014-04-28 19:24:02 -0300

    Adicionando arquivos faltantes do ultimo commit

---
+++ tests/Andreoav/Cbr/Case/AttributeTest.php
@@ -0,0 +1,47 @@
+<?php namespace Andreoav\Cbr\Cases;
+
+use Andreoav\Cbr\BaseTest;
+
+class AttributeTest extends BaseTest
+{
+	protected $_attribute;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->_attribute = new Attribute('attr', 100, 1.5);
+	}
+
+	public function testGetName()
+	{
+		$this->assertEquals($this->_attribute->getName(), 'attr');
+	}
+
+	public function testSetName()
+	{
+		$this->_attribute->setName('attr2');
+		$this->assertEquals($this->_attribute->getName(), 'attr2');
+	}
+
+	public function testGetValue()
+	{
+		$this->assertEquals($this->_attribute->getValue(), 100);
+	}
+
+	public function testSetValue()
+	{
+		$this->_attribute->setValue(150);
+		$this->assertEquals($this->_attribute->getValue(), 150);
+	}
+
+	public function testGetWeight()
+	{
+		$this->assertEquals($this->_attribute->getWeight(), 1.5);
+	}
+
+	public function testSetWeight()
+	{
+		$this->_attribute->setWeight(2.0);
+		$this->assertEquals($this->_attribute->getWeight(), 2.0);
+	}	
+}
---
+++ tests/Andreoav/Cbr/Case/CaseTest.php
@@ -0,0 +1,50 @@
+<?php namespace Andreoav\Cbr\Cases;
+
+use Andreoav\Cbr\BaseTest;
+
+class AbstractCaseTest extends BaseTest
+{
+	public function testLoadCase()
+	{
+		$cbrcase = new CBRCase;	
+		$cbrcase->loadCase(__DIR__ . '/testCase.json');
+		
+		// Assert case name
+		$this->assertEquals($cbrcase->getName(), 'Case Name');
+		
+		// Assert one attribute
+		$this->assertCount(2, $cbrcase->getAttributes());
+
+		// Assert attribute name
+		$this->assertEquals($cbrcase->getAttributes()[0]->getName(), 'attr');
+
+		// Assert value
+		$this->assertEquals($cbrcase->getAttributes()[0]->getValue(), 150);
+
+		// Assert weight
+		$this->assertEquals($cbrcase->getAttributes()[0]->getWeight(), 1.0);
+	}
+
+	public function testAttributeByName()
+	{
+		$cbrcase = new CBRCase;
+		$cbrcase->loadCase(__DIR__ . '/testCase.json');
+
+		// Test instance
+		$this->assertInstanceOf('Andreoav\Cbr\Cases\Attribute',
+			$cbrcase->getAttributeByName('attr'));
+
+		// Test attribute name
+		$this->assertEquals($cbrcase->getAttributeByName('attr')->getName(), 'attr');
+
+		// Test attribute value
+		$this->assertEquals($cbrcase->getAttributeByName('attr')->getValue(), 150);
+
+		// Test attribute weight
+		$this->assertEquals($cbrcase->getAttributeByName('attr')->getWeight(), 1.0);
+
+		// must be null
+		$this->assertNull($cbrcase->getAttributeByName('otherAttribute'));
+		
+	}
+}
